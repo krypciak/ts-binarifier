a96564545402c442c4ac31fae9b244e12ffd4cbd
krypek <krypek@tuta.io>
2025-09-17 14:49:20 +0200

    Create run config

--- src/run.ts
+++ src/run.ts
@@ -1,35 +1,20 @@
-import fs from 'fs'
-import { codeGen } from './code-gen'
-import { createProgram, findTypeForTypeDeclaration, getFile } from './type-extractor'
-import './colors'
-import { TypeParser } from './type-parser'
 import { customStuffNode } from './custom-handler'
+import { generateEncodeDecodeScripts } from './config'
 
 /* TODO: asserts in encoder */
 
-async function run() {
-    const projectRoot = '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery'
-    const filePath = 'src/server/physics/physics-server-sender.ts'
-    const typeName = 'GenerateType'
-    const outFile =
-        '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery/src/server/physics/bin.ts'
-
-    // const projectRoot = '/home/krypek/home/Programming/repos/binarifier'
-    // const filePath = '/src/index.test.ts'
-    // const typeName = 'Type4'
-    // const outFile = ''
-
-    const { program, checker } = await createProgram(projectRoot)
-
-    const file = getFile(program, filePath)
-    const { type, fullPath } = findTypeForTypeDeclaration(file, checker, typeName)
-
-    const parser = new TypeParser(checker, { customNodes: { customUnionRecord: customStuffNode } })
-    const node = parser.parseToNode(type)
-    console.log(node.print())
-
-    const code = codeGen(node, 'Gen', fullPath, typeName, outFile)
-    await fs.promises.writeFile(outFile, code)
-}
-
-await run()
+await generateEncodeDecodeScripts({
+    configs: [
+        {
+            projectRoot: '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery',
+            path: 'src/server/physics/physics-server-sender.ts',
+            typeType: 'type',
+            typeName: 'GenerateType',
+            outPath:
+                '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery/src/server/physics/bin.ts',
+            outClassName: 'PhysicsStatePacketEncoderDecoder',
+            printNode: true,
+            parserOptions: { customNodes: { customUnionRecord: customStuffNode } },
+        },
+    ],
+})
