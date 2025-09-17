4e45c1c110894330e8a24bd0d0e4e6e41d8f6081
krypek <krypek@tuta.io>
2025-09-17 17:23:43 +0200

    Update

--- src/run.ts
+++ src/run.ts
@@ -1,20 +1,28 @@
-import { customStuffNode } from './custom-handler'
 import { generateEncodeDecodeScripts } from './index'
 
 /* TODO: asserts in encoder */
 
 await generateEncodeDecodeScripts({
     configs: [
+        // {
+        //     projectRoot: '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery',
+        //     path: 'src/server/physics/physics-server-sender.ts',
+        //     typeType: 'type',
+        //     typeName: 'GenerateType',
+        //     outPath:
+        //         '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery/src/server/physics/bin.ts',
+        //     outClassName: 'PhysicsStatePacketEncoderDecoder',
+        //     printNode: true,
+        //     parserOptions: { customNodes: { customUnionRecord: customStuffNode } },
+        // },
         {
             projectRoot: '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery',
-            path: 'src/server/physics/physics-server-sender.ts',
+            path: 'src/server/remote/remote-server-sender.ts',
             typeType: 'type',
             typeName: 'GenerateType',
-            outPath:
-                '/home/krypek/home/Programming/crosscode/instances/cc-server/assets/mods/cc-multibakery/src/server/physics/bin.ts',
+            outPath: '',
             outClassName: 'PhysicsStatePacketEncoderDecoder',
             printNode: true,
-            parserOptions: { customNodes: { customUnionRecord: customStuffNode } },
         },
     ],
 })
